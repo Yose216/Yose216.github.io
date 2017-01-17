dd29843aef66091e55c66aba9e7b0e97d9495233
Yose <youness;chetoui@gmail.com>
2017-01-17 14:11:32 +0100

    add a smooth scroll if you click anchor

--- JS/script.js
+++ JS/script.js
@@ -107,3 +107,17 @@ $(document).ready(function(){
 		});
 	});
 });
+
+//Script for the scroll
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
