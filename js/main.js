60e4cee202c5439d5dd82ad76f59a5fb5731e647
Yose <youness.chetoui@gmail.com>
2017-01-31 10:15:39 +0100

    add a transition hover under my realisation

--- js/main.js
+++ js/main.js
@@ -11,3 +11,40 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
   }
 });
+
+$('.overlay').hover(
+       function(){ $('.titleReal').first().addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay2').hover(
+       function(){ $('.titleReal:eq(1)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay3').hover(
+       function(){ $('.titleReal:eq(2)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay4').hover(
+       function(){ $('.titleReal:eq(3)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay5').hover(
+       function(){ $('.titleReal:eq(4)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay6').hover(
+       function(){ $('.titleReal:eq(5)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay7').hover(
+       function(){ $('.titleReal:eq(6)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay8').hover(
+       function(){ $('.titleReal:eq(7)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
+$('.overlay9').hover(
+       function(){ $('.titleReal:eq(8)').addClass('hoverTitleReal').removeClass('titleReal') },
+       function(){ $('.hoverTitleReal').addClass('titleReal').removeClass('hoverTitleReal') }
+);
